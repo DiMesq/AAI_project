f7ecad6cfa558d902f427698380c677ce90a77d0
dmesquita <d.mesquita285@gmail.com>
2019-04-29 17:54:16 -0400

    Add script to generate the train and val labels for each image

---
+++ make_labels.py
@@ -0,0 +1,28 @@
+import glob
+import pandas as pd
+
+NUM_CLASSES = 964
+NUM_EXAMPLES = 20
+NUM_TRAIN_EXAMPLES = 18
+
+train_labels = []
+val_labels = []
+for i in range(1, NUM_CLASSES + 1):
+    for j in range(1, NUM_EXAMPLES + 1):
+        class_label = i - 1
+        class_label_str = f'{class_label:04d}'
+        character_id = f'{j:02d}'
+        image_id = f'{class_label_str}_{character_id}'
+        if j > NUM_TRAIN_EXAMPLES:
+            val_labels.append([image_id, class_label])
+        else:
+            train_labels.append([image_id, class_label])
+
+train_df = pd.DataFrame(train_labels, columns=['image_id', 'label'])
+train_df = train_df.sample(frac=1)
+val_df = pd.DataFrame(val_labels, columns=['image_id', 'label'])
+val_df = val_df.sample(frac=1)
+
+train_df.to_csv('data/train_labels.csv', index=False)
+val_df.to_csv('data/val_labels.csv', index=False)
+
